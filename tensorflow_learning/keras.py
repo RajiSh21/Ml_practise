d3dd1ca54c8076799dea340b4e19d945160e08e0
RajSh21 <rajishpandey508@gmail.com>
2026-03-03 22:33:47 +0545

    learning tensorflow from datacamp

---
+++ tensorflow_learning/keras.py
@@ -0,0 +1,32 @@
+# Define a Keras sequential model
+import keras
+
+
+model = keras.Sequential()
+
+# Define the first dense layer
+model.add(keras.layers.Dense(16, activation='relu', input_shape=(784,)))
+
+# Define the second dense layer
+model.add(keras.layers.Dense(8, activation='relu'))
+# Define the output layer
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Print the model architecture
+print(model.summary())
+
+
+# Define the first dense layer
+model.add(keras.layers.Dense(16, activation='sigmoid', input_shape=(784,)))
+
+# Apply dropout to the first layer's output
+model.add(keras.layers.Dropout(0.25))
+
+# Define the output layer
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Compile the model
+model.compile('adam', loss='categorical_crossentropy')
+
+# Print a model summary
+print(model.summary())
