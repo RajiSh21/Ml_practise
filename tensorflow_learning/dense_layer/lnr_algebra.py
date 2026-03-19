7823b4df5ffe9188bfaf11576eeb2950e7e1c70a
Rajish <162703585+RajiSh21@users.noreply.github.com>
2026-03-19 16:36:56 +0000

    dense layer

---
+++ tensorflow_learning/dense_layer/lnr_algebra.py
@@ -0,0 +1,25 @@
+# From previous step
+from uri_templateVariable,  impo
+from tensorflow import Variablert Variable
+
+from numpy import ones
+
+from tensorflow_learning.starting import keras, matmul
+
+
+bias1 = Variable(1.0)
+weights1 = Variable(ones((3, 2)))
+product1 = matmul(borrower_features, weights1)
+dense1 = keras.activations.sigmoid(product1 + bias1)
+
+# Initialize bias2 and weights2
+bias2 = Variable(ones((2,1)))
+weights2 = Variable(ones((2, 1)))
+
+# Perform matrix multiplication of dense1 and weights2
+product2 = matmul(dense1, weights2)
+
+# Apply activation to product2 + bias2 and print the prediction
+prediction = keras.activations.sigmoid(product2 + bias2)
+print('\n prediction: {}'.format(prediction.numpy()[0,0]))
+print('\n actual: 1')
