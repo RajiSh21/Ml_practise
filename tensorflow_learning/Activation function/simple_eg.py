a9d62876277c86edd69fdd7525b0b2d3d975b267
Rajish <162703585+RajiSh21@users.noreply.github.com>
2026-03-19 16:51:02 +0000

    chore: add latest workspace updates

---
+++ tensorflow_learning/Activation function/simple_eg.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
+yound, old = 0.3, 0.6
+low_bill, high_bill = 0.1, 0.5
+
+young_high =1.0*young + 2.0*high_bill
+young_low = 1.0*young = 2.0*low_bill
+old_high = 1.0*old + 2.0*high_bill
+old_low = 1.0*old + 2.0*low_bill
+
+print(young_high - young_low)
+
+
+print(old_high - old_low)
